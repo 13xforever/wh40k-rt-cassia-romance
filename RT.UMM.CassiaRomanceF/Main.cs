c37347f07075af7b3092596e7d32d1915ebcb187
13xforever <ilya.veselov@gmail.com>
2024-01-02 22:30:31 +0500

    initial commit

---
+++ RT.UMM.CassiaRomanceF/Main.cs
@@ -0,0 +1,47 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using UnityModManagerNet;
+
+namespace RT.UMM.CassiaRomanceF;
+
+#if DEBUG
+[EnableReloading]
+#endif
+static class Main
+{
+    internal static Harmony HarmonyInstance;
+    internal static UnityModManager.ModEntry.ModLogger log;
+    internal static bool enabled;
+
+    static bool Load(UnityModManager.ModEntry modEntry)
+    {
+        log = modEntry.Logger;
+#if DEBUG
+        modEntry.OnUnload = OnUnload;
+#endif
+        modEntry.OnToggle = OnToggle; 
+        modEntry.OnGUI = OnGUI;
+        HarmonyInstance = new(modEntry.Info.Id);
+        HarmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+        return true;
+    }
+
+    private static bool OnToggle(UnityModManager.ModEntry modEntry, bool state)
+    {
+        enabled = state;
+        return true;
+    }
+
+    static void OnGUI(UnityModManager.ModEntry modEntry)
+    {
+
+    }
+
+#if DEBUG
+    static bool OnUnload(UnityModManager.ModEntry modEntry)
+    {
+        HarmonyInstance.UnpatchAll(modEntry.Info.Id);
+        return true;
+    }
+#endif
+}
